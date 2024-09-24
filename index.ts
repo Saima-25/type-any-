59dc0f6f4f36c7c3dca4677ed66284563fcb88d3
unknown <maima.alive25@gmail.com>
2024-09-24 20:58:14 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+let a:any;
+a=4;
+a="Saima"
+a=true
+let a1:number=a
+let a2:string=a
+let a3:unknown=a
+let a4:undefined=a
+let a5:boolean=a
+let a6:null=a            // no error 'any' accept all kind of data type which are defined laterly
